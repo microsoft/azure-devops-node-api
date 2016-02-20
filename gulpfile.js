74c56c3c2f92e3377c7becb0c6dc9060ca92a8f6
Bryan MacFarlane <bryanmac@microsoft.com>
2016-02-20 05:47:14 -0500

    change gulp typescript compile

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,40 @@ var path = require('path');
 var gulp = require('gulp');
 var del = require('del');
 var mocha = require('gulp-mocha');
-var typescript = require('gulp-tsc');
+var gts = require('gulp-typescript');
+var typescript = require('typescript');
 var dtsgen = require('dts-generator');
 
 var srcDir = path.join(__dirname, 'api');
+
+var proj = gts.createProject('./tsconfig.json', { typescript: typescript });
+var ts = gts(proj);
+
+function errorHandler(err) {
+    console.error(err.message);
+    process.exit(1);
+}
 
 gulp.task('clean', function (done) {
     return del(['_build', '_def'], done);
 });
 
-// builds the whole api and drops definition files (.d.ts files) in place
 gulp.task('compile', ['clean'], function () {
-    return tsResult = gulp.src(['api/**/*.ts'])
-        .pipe(typescript({ removeComments: true, outDir: 'api' }))
+    return gulp.src(['./api/**/*.ts'], { base: './api'})
+        .pipe(ts)
         .pipe(gulp.dest('api'))
-        .on('error', function (err) { console.error(err.message);process.exit(1) });
+        .on('error', errorHandler)
 });
+
+// builds the whole api and drops definition files (.d.ts files) in place
+
+// gulp.task('compile', ['clean'], function () {
+//     return tsResult = gulp.src(['api/**/*.ts'])
+//         .pipe(typescript({ removeComments: true, outDir: 'api' }))
+//         .pipe(gulp.dest('api'))
+//         .on('error', function (err) { console.error(err.message);process.exit(1) });
+// });
+
 
 gulp.task('publishBuild', ['compile'], function () {
     return gulp.src(['api/**/*.js'])
@@ -36,7 +54,7 @@ gulp.task('publishDefinitions', ['compile'], function () {
 });
 
 gulp.task('copy', ['compile'], function () {
-    return gulp.src(['package.json', 'README.md'])
+    return gulp.src(['package.json', 'LICENSE', 'README.md'])
         .pipe(gulp.dest('_build'));
 });
 
