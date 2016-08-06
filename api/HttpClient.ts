c38eb2edfa5d9e0e63ae71b852f9e6dd836797c1
gregjhogan <gregjhogan@gmail.com>
2016-08-06 00:47:56 -0500

    fix tunnel module resolution

--- api/HttpClient.ts
+++ api/HttpClient.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+/// <reference path="../node/tunnel.d.ts"/>
+
 import url = require("url");
 
 import http = require("http");
