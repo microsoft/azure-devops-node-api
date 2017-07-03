dc1643cbc5476ce8a8553c754ab5635fe08cea4d
Konstantin Neradovsky <konstantin.neradovskiy@open.ru>
2017-07-03 20:45:19 +0300

    fixes #50

--- api/TestApi.ts
+++ api/TestApi.ts
@@ -3477,7 +3477,8 @@ export class TestApi extends basem.ClientApiBase implements ITestApi {
             let routeValues: any = {
                 project: project,
                 planId: planId,
-                suiteId: suiteId
+                suiteId: suiteId,
+		action: 'TestCases'
             };
 
             try {
