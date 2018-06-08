eb52b2b4c20f04f8ed5e544a32798cb06bd92d93
Giridharan Narayanan <ginara@microsoft.com>
2018-06-08 10:52:17 +0530

    Adding sample for Wiki API

--- samples/creation.ts
+++ samples/creation.ts
@@ -25,6 +25,7 @@ import { WidgetScope, WidgetTypesResponse } from 'vso-node-api/interfaces/Dashbo
 import { WorkItemField } from 'vso-node-api/interfaces/WorkItemTrackingInterfaces';
 import { ProcessModel } from 'vso-node-api/interfaces/WorkItemTrackingProcessInterfaces';
 import { PickListMetadataModel } from 'vso-node-api/interfaces/WorkItemTrackingProcessDefinitionsInterfaces';
+import { Wiki } from 'vso-node-api/interfaces/WikiInterfaces';
 
 // In order for this to run you will need to set the following environment variables:
 // 
@@ -203,6 +204,15 @@ export async function run() {
 
         if (changesets) {
             console.log(`found ${changesets.length} changesets`);
+        }
+
+        /********** Wiki **********/
+        printSectionStart('Wiki');
+        const wikiApi = await vstsCollectionLevel.getWikiApi();
+        const wikis: Wiki[] = await wikiApi.getAllWikis();
+
+        if (wikis) {
+            console.log(`found ${wikis.length} wikis`);
         }
 
         /********** Work **********/
