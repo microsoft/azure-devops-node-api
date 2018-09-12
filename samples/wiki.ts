0f463e29365d492ebe74cbdc178478ba05604a1a
Danny McCormick <damccorm@microsoft.com>
2018-09-12 16:17:02 -0400

    Responding to feedback

--- samples/wiki.ts
+++ samples/wiki.ts
@@ -7,13 +7,13 @@ import * as WikiApi from 'azure-devops-node-api/WikiApi';
 import * as WikiInterfaces from 'azure-devops-node-api/interfaces/WikiInterfaces';
 
 export async function run() { 
-    let webApi: nodeApi.WebApi = await common.getWebApi();
-    let wikiApiObject: WikiApi.IWikiApi = await webApi.getWikiApi();
-    let coreApiObject: CoreApi.ICoreApi = await webApi.getCoreApi();
+    const webApi: nodeApi.WebApi = await common.getWebApi();
+    const wikiApiObject: WikiApi.IWikiApi = await webApi.getWikiApi();
+    const coreApiObject: CoreApi.ICoreApi = await webApi.getCoreApi();
 
     common.banner('Wiki Samples');
-    let project: string = common.getProject();
-    let projectObject: CoreInterfaces.TeamProject = await coreApiObject.getProject(project);
+    const project: string = common.getProject();
+    const projectObject: CoreInterfaces.TeamProject = await coreApiObject.getProject(project);
     console.log('Project:', project);
 
     common.heading('Get all wikis');
@@ -21,24 +21,24 @@ export async function run() {
     console.log("Wikis", wikis.map((wiki) => wiki.name));
 
     let wikiId: string;
-    if (wikis.length > 0) {
-        wikiId = wikis[0].id;
-    }
-    else {
+    const createNewWiki: boolean = (wikis.length === 0);
+    if (createNewWiki) {
         common.heading("Create a wiki");
         const wikiParams: WikiInterfaces.WikiCreateParametersV2 = <WikiInterfaces.WikiCreateParametersV2>{name: "Hello Wiki", projectId: projectObject.id};
         const newWiki = await wikiApiObject.createWiki(wikiParams, project);
         console.log("Wiki created:", newWiki.name);
         wikiId = newWiki.id;
+    } else {
+        wikiId = wikis[0].id;
     }
 
     common.heading("Get the text from a wiki");
-    const textStream: NodeJS.ReadableStream = await wikiApiObject.getPageText(project, wikiId)
-    console.log("Wiki text", textStream.read().toString());
+    const pageText: NodeJS.ReadableStream = await wikiApiObject.getPageText(project, wikiId)
+    console.log("Wiki text", pageText.read().toString());
 
-    if (wikis.length == 0) {
+    if (createNewWiki) {
         common.heading("Delete the created wiki");
-        await wikiApiObject.deleteWiki(wikiId, project);
-        console.log("Wiki deleted");
+        const deletedWiki: WikiInterfaces.WikiV2 = await wikiApiObject.deleteWiki(wikiId, project);
+        console.log("Wiki", deletedWiki.name, "deleted");
     }
 }
