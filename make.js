591f0c6126bbe74a3038bfa95bc1b54802d605b4
Danny McCormick <damccorm@microsoft.com>
2019-01-18 11:15:08 -0500

    Fix individual sample execution (#284)
    * Clean up samples
    
    * Keep errors
    
    * Handle case where there are less than 4 args
    
    * Feedback

--- make.js
+++ make.js
@@ -67,7 +67,12 @@ target.samples = function() {
     run('tsc -p samples');
 
     pushd('samples');
-    run('node run.js');
+    if (process.argv[3]) {
+        run('node run.js ' + process.argv[3]);
+    }
+    else {
+        run('node run.js');
+    }
     popd();
     console.log('done');
 }
