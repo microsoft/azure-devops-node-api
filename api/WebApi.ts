2e02035b9e0db7d831da7f6fe89d6e879123bee0
emtencza <51380256+emtencza@users.noreply.github.com>
2019-06-04 12:14:04 -0700

    Fixed isBrowser check to use window.document instead (#319)
    * Fixed isBrowser check to use window.document instead
    
    * Simplified isBrowser check
    
    * Removed function from isBrowser

--- api/WebApi.ts
+++ api/WebApi.ts
@@ -42,8 +42,7 @@ import os = require('os');
 import url = require('url');
 import path = require('path');
 
-const isBrowser: boolean =(function(){return typeof window !== 'undefined' && this===window})();
-
+const isBrowser: boolean = typeof window !== 'undefined';
 /**
  * Methods to return handler objects (see handlers folder)
  */
