cfd9fcb81103c4a28e628e501456d4c5ecc7f437
Danny McCormick <damccorm@microsoft.com>
2019-06-10 16:56:10 -0400

    Revert #313 (#322)
    * Dont send version in userAgent
    
    * Revert #313

--- api/WebApi.ts
+++ api/WebApi.ts
@@ -145,7 +145,7 @@ export class WebApi {
                 userAgent = `${nodeApiName} (${window.navigator.userAgent})`;
             }
         } else {
-            const nodeApiVersion = require('../package.json').version;
+            const nodeApiVersion: string = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'package.json'), 'utf8')).version;
             const osName: string = os.platform();
             const osVersion: string = os.release();
 
