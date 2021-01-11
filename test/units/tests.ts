487e41350776c8583a70f29d28c6ee5106ecd1fc
Nikita Ezzhev <v-niezz@microsoft.com>
2021-01-11 14:13:09 +0300

    Remove asyncs

--- test/units/tests.ts
+++ test/units/tests.ts
@@ -301,32 +301,32 @@ describe('WebApi Units', function () {
 });
 
 describe('Auth Handlers Units', function () {
-    it('cross origin authenthication should be allowed by default in BasicCredentialHandler', async() => {
+    it('cross origin authenthication should be allowed by default in BasicCredentialHandler', () => {
         const basicAuthHandler = WebApi.getBasicHandler('user', 'password');
         assert(basicAuthHandler['allowCrossOriginAuthentication'] === true);
     });
 
-    it('cross origin authenthication should be allowed by default in BearerCredentialHandler', async() => {
+    it('cross origin authenthication should be allowed by default in BearerCredentialHandler', () => {
         const bearerAuthHandler = WebApi.getBearerHandler('0000000000000000000000000000000000000000');
         assert(bearerAuthHandler['allowCrossOriginAuthentication'] === true);
     });
 
-    it('cross origin authenthication should be allowed by default in PersonalAccessTokenCredentialHandler', async() => {
+    it('cross origin authenthication should be allowed by default in PersonalAccessTokenCredentialHandler', () => {
         const personalAccessTokenAuthHandler = WebApi.getPersonalAccessTokenHandler('0000000000000000000000000000000000000000');
         assert(personalAccessTokenAuthHandler['allowCrossOriginAuthentication']  === true);
     });
 
-    it('cross origin authenthication could be disabled in BasicCredentialHandler', async() => {
+    it('cross origin authenthication could be disabled in BasicCredentialHandler', () => {
         const basicAuthHandler = WebApi.getBasicHandler('user', 'password', false);  
         assert(basicAuthHandler['allowCrossOriginAuthentication'] === false);
     });
 
-    it('cross origin authenthication could be disabled in BearerCredentialHandler', async() => {
+    it('cross origin authenthication could be disabled in BearerCredentialHandler', () => {
         const bearerAuthHandler = WebApi.getBearerHandler('0000000000000000000000000000000000000000', false);
         assert(bearerAuthHandler['allowCrossOriginAuthentication'] === false);
     });
 
-    it('cross origin authenthication could be disabled in PersonalAccessTokenCredentialHandler', async() => {
+    it('cross origin authenthication could be disabled in PersonalAccessTokenCredentialHandler', () => {
         const personalAccessTokenAuthHandler = WebApi.getPersonalAccessTokenHandler('0000000000000000000000000000000000000000', false);
         assert(personalAccessTokenAuthHandler['allowCrossOriginAuthentication'] === false);
     });
