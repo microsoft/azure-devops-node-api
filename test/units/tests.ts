9c6604c9ed026b5a0b6766cf7597797e4c867199
Ilya Kuleshov <v-ikuleshov@microsoft.com>
2021-09-10 17:10:26 +0300

    add comment for a different strings

--- test/units/tests.ts
+++ test/units/tests.ts
@@ -181,6 +181,7 @@ describe('VSOClient Units', function () {
 
         //Assert
         assert.equal(res.apiVersion, '1');
+        //Use different strings for Node 6 and 8, because of a varied response string
         var expectedURL = semver.lt(process.versions.node, '8.0.0') ? 'https://dev.azure.com/testTemplate?min=Wed%2C%2019%20Oct%20%20208%2000%3A00%3A00%20GMT' : 'https://dev.azure.com/testTemplate?min=Wed%2C%2019%20Oct%200208%2000%3A00%3A00%20GMT';
         assert.equal(res.requestUrl, expectedURL);
     });
