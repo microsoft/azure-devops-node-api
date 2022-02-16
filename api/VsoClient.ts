9bbf15f02d274f323af18291e322ff0e3f15969f
Ivan Golubev <v-igolubev@microsoft.com>
2022-02-16 20:36:13 +0300

    Add check 'false' value in query params

--- api/VsoClient.ts
+++ api/VsoClient.ts
@@ -185,7 +185,7 @@ export class VsoClient {
     }
 
     private queryParamsToStringHelper(queryParams: any, prefix: string): string {
-        if (!queryParams) {
+        if (!queryParams && queryParams != false) {
             return '';
         }
         let queryString: string = '';
